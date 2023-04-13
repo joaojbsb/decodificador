7958dc12df54c0144c627090013fe290f9018640
João Oliveira Lima Júnior <joao.jbsb@gmail.com>
2023-04-13 10:15:38 -0300

    primeiro commit

---
+++ javascript/script.js
@@ -0,0 +1,28 @@
+
+let textInput = document.getElementById('text-area');
+
+let textOutput = document.getElementById('text-output');
+
+
+function codificar(event){
+    textEncoded = textInput.value.replace('e', 'enter').replace('i', 'imes').replace('a', "ai").replace('o', 'ober').replace('u', 'ufat');
+
+    console.log(textEncoded);
+
+    textOutput.value = textEncoded;
+
+    document.getElementById('text-not-found').style.display = 'none';
+    document.getElementById('imagem').style.display = 'none';
+    document.getElementById('text-output').style.height = '42rem';
+    document.getElementById('button-copy').style.display = 'block';
+ 
+}
+
+function descriptografar(event) {
+    textDescription = textInput.value.replace('enter', 'e').replace('imes', 'i').replace('ai', "a").replace('ober', 'o').replace('ufat', 'u');
+    
+    textOutput.value = textDescription;
+
+    document.getElementById('text-not-found').style.display = 'none';
+    document.getElementById('imagem').style.display = 'none';
+}
