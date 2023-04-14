75f1d1e2fce6598a8af709f0593038519bb0599a
João Oliveira Lima Júnior <joao.jbsb@gmail.com>
2023-04-14 12:11:10 -0300

    Aumentando e diminuindo o tamanho da fonte

---
+++ README.md
@@ -0,0 +1,23 @@
+<h1 style="text-align: center; margin-top: 32px; margin-bottom: 32px;">Decodificador de texto</h1>
+
+
+<div style="text-align: center;">
+    <h2>Tecnologias utilizadas</h2>
+
+<ul>
+    <li style="list-style: none;">HTML</li>
+    <li style="list-style: none;">CSS</li>
+    <li style="list-style: none;">JavaScript</li>
+</ul>
+
+<img src="./assets/imagens/home.gif" alt="imagem da tela home">
+</div>
+
+<h2>Funcionalidades</h2>
+<ul>
+<li>Criptografa e descriptogra o texto digitado</li>
+<li>Tem o modo noturno</li>
+<li>Aumenta e diminui o tamanho da fonte</li>
+</ul>
+
+
--- javascript/script.js
+++ javascript/script.js
@@ -16,7 +16,7 @@ function codificar(event){
     document.getElementById('text-output').style.height = '42rem';
     document.getElementById('button-copy').style.display = 'block';
  
-}
+};
 
 function descriptografar(event) {
     textDescription = textInput.value.replace('enter', 'e').replace('imes', 'i').replace('ai', "a").replace('ober', 'o').replace('ufat', 'u');
@@ -25,19 +25,48 @@ function descriptografar(event) {
 
     document.getElementById('text-not-found').style.display = 'none';
     document.getElementById('imagem').style.display = 'none';
-}
+};
 
 function dark() {
     const bodyDark = document.getElementById('body');
     const textArea = document.getElementById('text-area');
 
-    if (bodyDark.classList.value == 'dark') {
-        bodyDark.classList.remove('dark');
-        textArea.classList.remove('dark');
-    } else {
+    if (bodyDark.classList.value == '' | bodyDark.classList.value == 'increment-font' | bodyDark.classList.value == 'decrement-font') {
+
         bodyDark.classList.add('dark');
         textArea.classList.add('dark');
-    }
+        return
+    } 
+    bodyDark.classList.remove('dark');
+    textArea.classList.remove('dark');
+    
 
     console.log(bodyDark.classList);
+};
+
+function copiar() {
+    const text = document.getElementById('text-output');
+
+    text.select();
+
+    document.execCommand('copy');
+};
+
+function incrementFont () {
+    const font = document.getElementById('body');
+
+    body.classList.remove('decrement-font');
+    body.classList.add('increment-font');
+};
+
+function decrementFont (){
+    const font = document.getElementById('body');
+
+    body.classList.remove('increment-font');
+    body.classList.add('decrement-font');
+};
+
+function font () {
+    body.classList.remove('increment-font');
+    body.classList.remove('decrement-font');
 }
